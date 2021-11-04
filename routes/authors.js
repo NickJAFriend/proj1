62c5c9cf0d0ff8a408e78df3cdc23100715778b8
NickJAFriend <nick@bigriverconcepts.com>
2021-11-04 10:43:17 +0000

    Bug Foun in authors.js - working here

--- routes/authors.js
+++ routes/authors.js
@@ -2,16 +2,36 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
 
+
 // All Authors Route
 router.get('/', (req,res)=> {
     res.render('authors/index')
 })
+
 // New Authors Route
 router.get('/new', (req,res)=> {
     res.render('authors/new', { author: new Author()})
 })
+
 //Create Author Route
 router.post('/', (req, res)=> {
+    /*
+    const author = new Author({
+        name: req.body.name
+    })
+
+    author.save((err, newAuthor) => {
+        if (err){
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'error creating author'
+            })
+        }else {
+            //res.redirect(`authors/${newAuthor.id}`)
+            res.redirect(`authors/`)
+        }
+    })
+    */
     res.send(req.body.name)
 })
 
--- server.js
+++ server.js
@@ -6,16 +6,14 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
-
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.use(express.json());
 app.use(express.urlencoded({limit: '10mb', extended: false}));
